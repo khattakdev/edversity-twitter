46232d9e053574a070a16df912bb16a331c05367
Arsalan Khattak <me@khattak.dev>
2024-01-01 19:34:01 +0500

    inial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+// import Counter from "./components/Counter";
+
+import "./App.css";
+function App() {
+  // const [count, setCount] = useState(0);
+  return (
+    <>
+      <h1>Edverter</h1>
+
+      {/* <Counter count={count} setCount={setCount} /> */}
+    </>
+  );
+}
+
+export default App;
