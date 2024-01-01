eac28b0013921fce165d8d483bb1da50749f7d65
Arsalan Khattak <me@khattak.dev>
2024-01-01 21:12:37 +0500

    c4 final

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
+import { useState } from "react";
 import Home from "./pages/Home";
-// import Auth from "./pages/Auth";
+import Auth from "./pages/Auth";
 
 import "./App.css";
 function App() {
   return (
     <>
       <Home />
-      {/* <Auth /> */}
     </>
   );
 }
