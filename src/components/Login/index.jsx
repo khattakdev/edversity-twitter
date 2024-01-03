23dc953c3de8ab8ed38914d54b4555e06140da5a
Arsalan Khattak <me@khattak.dev>
2024-01-03 20:57:13 +0500

    basic form validation

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,11 +1,37 @@
+import { useState } from "react";
 import classes from "./index.module.css";
 function Login({ loginState }) {
+
+  const [email, SetEmail] = useState("");
+  const [password, SetPassword] = useState("");
+  const [errorMessage, SetErrorMessage] = useState("");
+
+  const onLogin = (e) => {
+    e.preventDefault();
+    const validEmail = email.trim();
+    
+    if (!validEmail) {
+      SetErrorMessage("Please write your email!")
+    } else {
+      SetErrorMessage("Please write your password")
+    }
+
+    SetErrorMessage("")
+    console.log("Email - ", email )
+    console.log("Password - ", password)
+
+  }
+
+
   return (
     <div className={classes.login}>
       <h1>Login to your account</h1>
-      <form className={classes.login_form}>
-        <input className={classes.input_field} type="email" placeholder="Your email" />
+       <p>{errorMessage}</p>
+      <form onSubmit={(e) => { onLogin(e)}} className={classes.login_form}>
+        <input value={email} onChange={(e) => { SetEmail(e.target.value)}} className={classes.input_field} type="email" placeholder="Your email" />
         <input
+        value={password}
+        onChange={(e) => { SetPassword(e.target.value)}}
           className={classes.input_field}
           type="password"
           placeholder="Your password"
