5740f5a32e77d3131a187e1e868a091406819160
Arsalan Khattak <me@khattak.dev>
2024-01-19 20:31:12 +0500

    add links

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,14 +1,37 @@
+import { Link } from "react-router-dom";
 import classes from "./index.module.css";
 function Navigation() {
+  const navLinks = [
+    {
+      placeholder: "Home",
+      path: "/home",
+    },
+    {
+      placeholder: "Followers",
+      path: "/home/followers",
+    },
+    {
+      placeholder: "Followings",
+      path: "/home/followings",
+    },
+    {
+      placeholder: "Settings",
+      path: "/setting",
+    },
+    {
+      placeholder: "sign out",
+      path: "/",
+    },
+  ];
   return (
     <nav>
       <h3 className="text-xl uppercase font-semibold">Edverter</h3>
       <ul className={classes.list_items}>
-        <li className={classes.list_item}>Home</li>
-        <li className={classes.list_item}>Followers</li>
-        <li className={classes.list_item}>Followings</li>
-        <li className={classes.list_item}>Settings</li>
-        <li className={classes.list_item}>Sign out</li>
+        {navLinks.map((navlink, index) => (
+          <li className={classes.list_item} key={index}>
+            <Link to={navlink.path}>{navlink.placeholder}</Link>
+          </li>
+        ))}
       </ul>
     </nav>
   );
